a6cf962248ce7ae22eaf71b566a4a36bb02dd0a4
liangyusen <liangyusen1202@gmail.com>
2017-10-28 20:50:22 +0800

    auto

--- mx_web_front/app/src/pages/list.vue
+++ mx_web_front/app/src/pages/list.vue
@@ -1,14 +1,61 @@
 <template>
   <div class="page-list container">
-    <h1 @click="goPay">list</h1> 
-    
+    <h3 class="header">
+      <span @click="goBack" class="btn-go-back">取消</span>
+    </h3>
+    <div class="body">
+      <ul class="nav">
+        <li class="item cur" @click="filterList('sd')">item1</li>
+        <li class="item" @click="filterList('ds')">item2</li>
+        <li class="item" @click="filterList('ds')">item3</li>
+
+      </ul>
+      <div class="list-container">
+        <h4 class="tit">套餐</h4>
+        <ul class="list">
+          <li class="item" v-for="(item, index) in goods" :key="index">
+            <div class="wrap-pic">
+              <img :src="item.url" alt="">
+            </div>
+          </li>
+          <li class="item" v-for="(item, index) in goods" :key="index">
+            <div class="wrap-pic">
+              <img :src="item.url" alt="">
+            </div>
+          </li>
+          <li class="item" v-for="(item, index) in goods" :key="index">
+            <div class="wrap-pic">
+              <img :src="item.url" alt="">
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 <script>
 export default {
+  data: function () {
+    return {
+      goods: [
+        {
+          url: '/tmp/tmp_item_0.png',
+          name: '100ml豆浆加1个水煮鸡蛋',
+
+        }
+      ]
+    }
+  },
   methods: {
     goPay() {
       this.$router.push('pay')
+    },
+    goBack() {
+      history.back()
+    },
+    filterList(type) {
+      console.log(type)
     }
   }
 }
