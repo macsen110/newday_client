d7e02063dd20f4340f5f9dc11faf50b003ffad3e
liangyusen <liangyusen1202@gmail.com>
2017-12-14 19:01:28 +0800

    添加page

---
+++ pages/page.js
@@ -0,0 +1,58 @@
+(function () {
+  var page = {
+    _config: {
+      mode: 'hash'
+    },
+    _init: function (options) {
+      if (typeof options === 'object') {
+        for (var i in options) this._config[i] = options[i]
+      }
+      this._registerEvent()
+    },
+    _registerEvent: function () {
+
+    },
+    _route: function () {
+
+    },
+    sameOrigin: sameOrigin
+  },
+  /**
+   * Bind with the given `options`.
+   *
+   * Options:
+   *
+   *    - `click` bind to click events [true]
+   *    - `popstate` bind to popstate [true]
+   *    - `dispatch` perform initial dispatch [true]
+   *
+   * @param {Object} options
+   * @api public
+   */
+
+  page.start = function(options) {
+    options = options || {};
+    if (running) return;
+    running = true;
+    if (false === options.dispatch) dispatch = false;
+    if (false === options.decodeURLComponents) decodeURLComponents = false;
+    if (false !== options.popstate) window.addEventListener('popstate', onpopstate, false);
+    if (false !== options.click) {
+      document.addEventListener(clickEvent, onclick, false);
+    }
+    if (true === options.hashbang) hashbang = true;
+    if (!dispatch) return;
+    var url = (hashbang && ~location.hash.indexOf('#!')) ? location.hash.substr(2) + location.search : location.pathname + location.search + location.hash;
+    page.replace(url, null, true, dispatch);
+  };
+  /**
+    * Check if `href` is the same origin.
+  */
+
+  function sameOrigin(href) {
+    var origin = location.protocol + '//' + location.hostname;
+    if (location.port) origin += ':' + location.port;
+    return (href && (0 === href.indexOf(origin)));
+  }
+  window.page = pages._init;
+}())
