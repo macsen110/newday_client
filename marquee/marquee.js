a25a4ed028faea44581dde7f4650c39504ba2bd5
macsen110 <839945193@qq.com>
2018-12-17 01:37:06 +0800

    update marquee

--- marquee/marquee.js
+++ marquee/marquee.js
@@ -1,7 +1,7 @@
 define([
 ], function () {
     'use strict';
-    function __marquee (dom, options) {
+    function __marquee(dom, options) {
         this.__init(dom, options)
     }
     __marquee.prototype = {
@@ -20,70 +20,120 @@ define([
             var _boxNums = Math.ceil(_rowNums / _limit);
             var _ul = '<ul class="marquee-box"></ul>';
             dom.innerHTML = _ul;
+            this.curMarqueeBox = dom.querySelector('.marquee-box');
             for (var _i = 0; _i < _boxNums; _i++) {
                 var _startPos = _i * 5;
-                var _endPos = (_i+ 1) * 5 >= _rowNums ? _rowNums : (_i+ 1) * 5;
+                var _endPos = (_i + 1) * 5 >= _rowNums ? _rowNums : (_i + 1) * 5;
                 this.__contentArr.push(_content.slice(_startPos, _endPos))
             }
-            this.__renderRowItems(dom.querySelector('.marquee-box'), 0)
-            
+            this.__renderRowItems(this.curMarqueeBox, 0)
+
         },
         __renderRowItems: function (curMarqueeBox, curListIdx) {
-            if (curListIdx === this.__contentArr.length) curListIdx = 0
+
             var _this = this;
             var curMarqueeList = _this.__contentArr[curListIdx]
             var _items = '';
             for (var _i = 0; _i < curMarqueeList.length; _i++) {
                 _items += '<li class="marquee-row">'
-                    + '<div class="marquee-row-item">' + curMarqueeList[_i].row + '</div>'
+                    + '<div class="marquee-item-wrap"><div class="marquee-row-item">' + curMarqueeList[_i].row + '</div></div>'
+                    + '<div class="marquee-item-shadow"></div>'
                     + '</li>'
             }
             curMarqueeBox.innerHTML = _items;
             this.__bindUI(curMarqueeBox, curListIdx)
         },
         __bindUI: function (curMarqueeBox, curListIdx) {
-            this.__loopRowTimeList = []
             this.__marqueeRowAddVisible(curMarqueeBox, curListIdx);
         },
         __marqueeRowAddVisible: function (curMarqueeBox, curListIdx) {
             var _this = this;
-            _this.curMarqueeBox = curMarqueeBox;
+            this.__loopRowTimeList = []
             setTimeout(function () {
                 curMarqueeBox.querySelectorAll('.marquee-row-item').forEach(function (element, idx) {
                     var _w = element.scrollWidth;
                     if (_w > element.parentElement.clientWidth) {
                         element.style.width = _w + 'px'
                         var diffWidth = _w - element.parentElement.clientWidth
-                        _this.__loopRowTimeList.push({idx: idx, diffWidth: diffWidth, element})
+                        _this.__loopRowTimeList.push({ idx: idx, diffWidth: diffWidth, element })
                     }
 
                 });
-                if (_this.__loopRowTimeList.length > 0) _this.__startScrollLeft(_this.__loopRowTimeList[0], 0,curListIdx)
+                if (_this.__loopRowTimeList.length > 0) _this.__startScrollLeft(_this.__loopRowTimeList[0], 0, curListIdx)
+                else {
+
+                    setTimeout(function () {
+                        if (_this.__contentArr.length > 1) {
+                            _this.__loopScrollUp(curListIdx)
+                        }
+                    }, 2000)
+                }
             }, 1000)
         },
         __loopRowTimeList: [],
         __loopScrollUp: function (curListIdx) {
-            this.__renderRowItems(this.__container.querySelector('.marquee-box'), curListIdx+1)
+            var _this = this;
+            var __contentArrLen = _this.__contentArr.length
+            var nextListIdx = curListIdx + 1 === __contentArrLen ? 0 : curListIdx + 1;
+            if (__contentArrLen === 1) return _this.__startScrollLeft(_this.__loopRowTimeList[0], 0, curListIdx)
+            var nextLoopList = _this.__contentArr[nextListIdx];
+            [].forEach.call(_this.curMarqueeBox.children, function (item, idx) {
+                setTimeout(function () {
+                    var element = item.querySelector('.marquee-row-item');
+                    var _newCon = (nextLoopList[idx] && nextLoopList[idx].row) || ''
+                    element.style.width = '100%'
+                    setTimeout(function () {
+                        if (!_newCon) item.classList.add('hidden');
+                        else {
+                            item.classList.remove('hidden')
+                            _this.__startFlip(item)
+                        }
+                        element.innerHTML = _newCon
+                    }, 500)
+                    if (idx === _this.curMarqueeBox.children.length -1) {
+                        setTimeout(function () {
+                            _this.__marqueeRowAddVisible(_this.curMarqueeBox, nextListIdx)
+                        }, 1000)
+                    }
+                }, (idx + 1) * 1000)
+            })
+        },
+        __startFlip: function (element) {
+            var start = null;
+            function step(timestamp) {
+                if (!start) start = timestamp;
+                var progress = timestamp - start;
+                element.style.transform = 'rotateX(-' + Math.min(360, progress / (1000 / 360)) + 'deg)';
+                if (progress < 1000) {
+                    window.requestAnimationFrame(step);
+                }
+                else {
+                    window.cancelAnimationFrame(step)
+                }
+            }
+            window.requestAnimationFrame(step)
         },
         __resetScrollLeft: function (options, idx, curListIdx) {
             var _this = this;
             var element = options.element;
-            element.style.transform = 'translateX(0)';
-            if ((idx + 1) !== _this.__loopRowTimeList.length) this.__startScrollLeft(_this.__loopRowTimeList[idx+1], idx+1, curListIdx)
-            else this.__loopScrollUp(curListIdx)
-        },
-        __nextScrollLeft: function (idx) {
-            
-
+            var _marqueeItem = element.parentElement.parentElement;
+            setTimeout(function () {
+                _marqueeItem.classList.remove('cur');
+                element.style.transform = 'translateX(0)';
+                if ((idx + 1) !== _this.__loopRowTimeList.length) _this.__startScrollLeft(_this.__loopRowTimeList[idx + 1], idx + 1, curListIdx)
+                else _this.__loopScrollUp(curListIdx)
+            }, 2000)
         },
         __startScrollLeft: function (options, idx, curListIdx) {
             var start = null;
             var element = options.element;
+            var _marqueeItem = element.parentElement.parentElement;
             var _this = this;
+            _marqueeItem.classList.add('cur');
             function step(timestamp) {
                 if (!start) start = timestamp;
                 var progress = timestamp - start;
-                element.style.transform = 'translateX(-' + Math.min(progress / (1000/options.diffWidth), options.diffWidth) + 'px)';
+                element.style.transform = 'translateX(-' + Math.min(progress / (1000 / options.diffWidth), options.diffWidth) + 'px)';
                 if (progress < 1000) {
                     window.requestAnimationFrame(step);
                 }
@@ -92,8 +142,9 @@ define([
                     _this.__resetScrollLeft(options, idx, curListIdx)
                 }
             }
-
-            window.requestAnimationFrame(step);
+            setTimeout(function () {
+                window.requestAnimationFrame(step)
+            }, 2000);
         }
 
     }
