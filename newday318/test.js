8cdf58b579f799772ceae2bc1e13fc1c1ff47452
liangyusen <liangyusen1202@gmail.com>
2019-09-06 17:23:54 +0800

    UPDATE WX LOGIN

--- newday318/test.js
+++ newday318/test.js
@@ -1,5 +1,5 @@
 var appid = "wx0b445a074d8f36a4";
 var scope = "snsapi_userinfo";
 var redirect_url = encodeURIComponent(location.href)
-var redirect_uri = encodeURIComponent('https://www.macsen318.com/api/oauth2?redirect_url='+redirect_url)
+var redirect_uri = encodeURIComponent('http://chou.1qigou.net/api/crowdfunding/wx/oauth2?redirect_url='+redirect_url)
 if (location.href.indexOf('code') === -1 )location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appid+"&redirect_uri="+redirect_uri+"&response_type=code&scope="+scope+"&state=STATE#wechat_redirect"
